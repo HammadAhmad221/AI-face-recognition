38fcd5ea4ea171721abe30ef7b5ec89d94fed874
HammadAhmad221 <hammadchahal123@gmail.com>
2024-09-19 15:29:50 -0700

    fixes

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   const [open, setOpen] = useState(false);
   const[data ,setData]=useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
-  // const [videoSrc, setVideoSrc] = useState('');
+  const [videoSrc, setVideoSrc] = useState('');
 
   useEffect(() => {
     const eventSource = new EventSource('http://3.111.29.237:80/detect_faces');
@@ -13,7 +13,7 @@ const HomePage = () => {
     eventSource.onmessage = (event) => {
       const newData = JSON.parse(event.data);
       console.log(newData);
-      setData((prevData) => [...prevData, ...newData]);
+      setData((prevData) => [...newData,...prevData]);
       console.log(newData);
     };
 
@@ -37,15 +37,15 @@ const HomePage = () => {
     setOpen(false);
   };
 
-  // useEffect(()=>{
-  //   setVideoSrc('http://3.111.29.237:80/video_feed');
-  // },[])
+  useEffect(()=>{
+    setVideoSrc('http://3.111.29.237:80/video_feed');
+  },[])
 
   return (
     <Box sx={{ padding: 2 }}>
-      {/* <Grid container spacing={2}> */}
+      <Grid container spacing={2}>
         {/* Full-Width Video */}
-        {/* <Grid item xs={12}>
+        <Grid item xs={12}>
           <Box sx={{ position: 'relative' }}>
             <img
               src={videoSrc}
@@ -57,18 +57,18 @@ const HomePage = () => {
               }}
             />
           </Box>
-        </Grid> */}
+        </Grid>
 
         {/* Table */}
         <Grid item xs={12}>
-          <TableContainer component={Paper} sx={{ maxHeight: '900px', overflowY: 'auto' }}>
+          <TableContainer component={Paper} sx={{ maxHeight: '330px', overflowY: 'auto' }}>
             <Table stickyHeader>
               <TableHead>
                 <TableRow>
                   <TableCell>Image</TableCell>
                   <TableCell>Name</TableCell>
                   <TableCell>Date</TableCell>
-                  {/* <TableCell>Time</TableCell> */}
+                  <TableCell>Time</TableCell>
                   {/* <TableCell>Status</TableCell> */}
                 </TableRow>
               </TableHead>
@@ -85,8 +85,8 @@ const HomePage = () => {
                       />
                     </TableCell>
                     <TableCell>{row.name}</TableCell>
-                    <TableCell>{row.timestamp}</TableCell>
-                    {/* <TableCell>{row.time}</TableCell> */}
+                    <TableCell>{row.date}</TableCell>
+                    <TableCell>{row.time}</TableCell>
                     {/* <TableCell>{row.status}</TableCell> */}
                   </TableRow>
                 ))}
@@ -94,7 +94,7 @@ const HomePage = () => {
             </Table>
           </TableContainer>
         </Grid>
-      {/* </Grid> */}
+      </Grid>
 
       {/* Modal */}
       <Dialog
@@ -112,9 +112,9 @@ const HomePage = () => {
                 <Avatar src={`data:image/jpeg;base64,${selectedUser.face_image}`} alt={selectedUser.name} sx={{ width: 120, height: 120 }} />
                 <Box sx={{ marginLeft: 2 }}>
                   <Typography variant="h6">{selectedUser.name}</Typography>
-                  {/* <Typography variant="body1">Status: {selectedUser.status}</Typography>
-                  <Typography variant="body1">Date: {selectedUser.date}</Typography> */}
-                  <Typography variant="body1">Time: {selectedUser.timestamp}</Typography>
+                  {/* <Typography variant="body1">Status: {selectedUser.status}</Typography> */}
+                  <Typography variant="body1">Date: {selectedUser.date}</Typography>
+                  <Typography variant="body1">Time: {selectedUser.time}</Typography>
                 </Box>
               </Box>
             </>
