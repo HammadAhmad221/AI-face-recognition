e9a6a084ad09f809f5daaf95b7830544bb8f0d87
HammadAhmad221 <hammadchahal123@gmail.com>
2024-09-27 18:27:01 -0700

    fix responsiveness and hide scrollbar

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -170,70 +170,75 @@ const LandingPage = () => {
           </div>
         </div>
       </div> */}
-      <div className="w-full h-[750px] bg-[#130D44] lg:flex flex-col items-center overflow-y-auto sm:hidden">
-      <div className="pt-[16.50px] min-h-[750px] flex flex-col items-center justify-center gap-[48.50px]">
-        <div className="self-stretch flex flex-col justify-start items-center gap-6">
-          <div className="self-stretch text-center">
-            <span className="text-white text-6xl font-semibold font-poppins">
-              Empowering <br /> Businesses with &nbsp;
-            </span>
-            <span             
-            style={{
+      <div className="w-full h-[750px] bg-[#130D44] lg:flex flex-col items-center overflow-y-auto scrollbar-hide max-[1000px]:hidden">
+        <div className="pt-[16.50px] min-h-[750px] flex flex-col items-center justify-center gap-[48.50px]">
+          <div className="self-stretch flex flex-col justify-start items-center gap-6">
+            <div className="self-stretch text-center">
+              <span className="text-white text-6xl font-semibold font-poppins">
+                Empowering <br /> Businesses with &nbsp;
+              </span>
+              <span
+                style={{
                   background:
                     "linear-gradient(90deg, #7266BE 0%, #63B1A7 64.58%, #42C09A 100%)",
                   backgroundClip: "text",
                   WebkitBackgroundClip: "text",
                   WebkitTextFillColor: "transparent",
                   marginTop: "24px",
-                }} className="text-6xl font-semibold font-poppins">
-              AI-
-              <br />
-              PPE Solutions
-            </span>
+                }}
+                className="text-6xl font-semibold font-poppins"
+              >
+                AI-
+                <br />
+                PPE Solutions
+              </span>
+            </div>
+            <div className="w-[50%] text-center text-white text-xl font-medium font-poppins leading-normal">
+              Our modules streamline attendance tracking and PPE detection,
+              giving you the tools to enhance safety and productivity.
+            </div>
           </div>
-          <div className="w-[50%] text-center text-white text-xl font-medium font-poppins leading-normal">
-            Our modules streamline attendance tracking and PPE detection,
-            giving you the tools to enhance safety and productivity.
-          </div>
-        </div>
-        <div className="flex flex-col justify-start items-start">
-          <div className="px-5 py-2.5 bg-gradient-to-r from-[#534998] to-[#42c09a] rounded-[10px] shadow border-2 flex flex-col justify-center items-center">
-            <div className="rounded flex justify-start items-center gap-2">
-              <div className="text-center text-white text-2xl font-normal font-poppins">
-                Schedule a Demo
+          <div className="flex flex-col justify-start items-start">
+            <div className="px-5 py-2.5 bg-gradient-to-r from-[#534998] to-[#42c09a] rounded-[10px] shadow border-2 flex flex-col justify-center items-center">
+              <div className="rounded flex justify-start items-center gap-2">
+                <div className="text-center text-white text-2xl font-normal font-poppins">
+                  Schedule a Demo
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="w-full min-h-[750px] flex justify-end items-center relative px-32">
-        <div>
-        <div className="absolute z-10 lg:left-20 lg:bottom-80">
-            <span className="text-white text-5xl font-semibold font-poppins">
-            AI-Driven PPE Monitoring:
-            </span><br />
-            <span             
-            style={{
+        <div className="w-full min-h-[750px] flex justify-end items-center relative px-32">
+          <div>
+            <div className="absolute z-10 lg:left-20 lg:bottom-80">
+              <span className="text-white text-5xl font-semibold font-poppins">
+                AI-Driven PPE Monitoring:
+              </span>
+              <br />
+              <span
+                style={{
                   background:
                     "linear-gradient(90deg, #7266BE 0%, #63B1A7 64.58%, #42C09A 100%)",
                   backgroundClip: "text",
                   WebkitBackgroundClip: "text",
                   WebkitTextFillColor: "transparent",
                   marginTop: "24px",
-                }} className="text-5xl font-semibold font-poppins">
-                  Enhancing Safety with <br /> Precision
-            </span>
+                }}
+                className="text-5xl font-semibold font-poppins"
+              >
+                Enhancing Safety with <br /> Precision
+              </span>
+            </div>
           </div>
+          <video
+            autoPlay
+            loop
+            muted
+            src="vedio1.mp4"
+            type="video/mp4"
+            className=" w-full h-auto max-w-[750px] rounded-tl-[160px] rounded-br-[160px] shadow-lg"
+          ></video>
         </div>
-        <video
-          autoPlay
-          loop
-          muted
-          src="vedio1.mp4"
-          type="video/mp4"
-          className=" w-full h-auto max-w-[750px] rounded-tl-[160px] rounded-br-[160px] shadow-lg"
-        ></video>
-      </div>
       </div>
 
       {/* rest code */}
@@ -341,69 +346,74 @@ const LandingPage = () => {
           </div>
         </div>
       </div> */}
-            <div className="w-full h-[750px] bg-[#130D44] lg:flex flex-col items-center overflow-y-auto sm:hidden">
-      <div className="pt-[16.50px] min-h-[750px] flex flex-col items-center justify-center gap-[48.50px]">
-        <div className="self-stretch flex flex-col justify-start items-center gap-6">
-          <div className="self-stretch text-center">
-            <span className="text-white text-6xl font-semibold font-poppins">
-              Empowering <br /> Businesses with &nbsp;
-            </span>
-            <span             
-            style={{
+      <div className="w-full h-[750px] bg-[#130D44] lg:flex flex-col items-center overflow-y-auto scrollbar-hide max-[1000px]:hidden">
+        <div className="pt-[16.50px] min-h-[750px] flex flex-col items-center justify-center gap-[48.50px]">
+          <div className="self-stretch flex flex-col justify-start items-center gap-6">
+            <div className="self-stretch text-center">
+              <span className="text-white text-6xl font-semibold font-poppins">
+                Empowering <br /> Businesses with &nbsp;
+              </span>
+              <span
+                style={{
                   background:
                     "linear-gradient(90deg, #7266BE 0%, #63B1A7 64.58%, #42C09A 100%)",
                   backgroundClip: "text",
                   WebkitBackgroundClip: "text",
                   WebkitTextFillColor: "transparent",
                   marginTop: "24px",
-                }} className="text-6xl font-semibold font-poppins">
-              AI- <br />Attendance Solutions
-            </span>
-          </div>
-          <div className="w-[50%] text-center text-white text-xl font-medium font-poppins leading-normal">
+                }}
+                className="text-6xl font-semibold font-poppins"
+              >
+                AI- <br />
+                Attendance Solutions
+              </span>
+            </div>
+            <div className="w-[50%] text-center text-white text-xl font-medium font-poppins leading-normal">
               Our modules simplify attendance tracking and optimize efficiency,
               providing you with the tools to enhance safety and boost
               productivity.
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col justify-start items-start">
-          <div className="px-5 py-2.5 bg-gradient-to-r from-[#534998] to-[#42c09a] rounded-[10px] shadow border-2 flex flex-col justify-center items-center">
-            <div className="rounded flex justify-start items-center gap-2">
-              <div className="text-center text-white text-2xl font-normal font-poppins">
-                Schedule a Demo
+          <div className="flex flex-col justify-start items-start">
+            <div className="px-5 py-2.5 bg-gradient-to-r from-[#534998] to-[#42c09a] rounded-[10px] shadow border-2 flex flex-col justify-center items-center">
+              <div className="rounded flex justify-start items-center gap-2">
+                <div className="text-center text-white text-2xl font-normal font-poppins">
+                  Schedule a Demo
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="w-full min-h-[750px] flex justify-end items-center relative px-32">
-        <div>
-        <div className="absolute z-10 lg:left-20 lg:bottom-80">
-            <span className="text-white text-5xl font-semibold font-poppins">
-            AI-Driven Attendance <br /> Tracking:
-            </span>
-            <span             
-            style={{
+        <div className="w-full min-h-[750px] flex justify-end items-center relative px-32">
+          <div>
+            <div className="absolute z-10 lg:left-20 lg:bottom-80">
+              <span className="text-white text-5xl font-semibold font-poppins">
+                AI-Driven Attendance <br /> Tracking:
+              </span>
+              <span
+                style={{
                   background:
                     "linear-gradient(90deg, #7266BE 0%, #63B1A7 64.58%, #42C09A 100%)",
                   backgroundClip: "text",
                   WebkitBackgroundClip: "text",
                   WebkitTextFillColor: "transparent",
                   marginTop: "24px",
-                }} className="text-5xl font-semibold font-poppins">
-                   Optimize <br /> Workforce Management
-            </span>
+                }}
+                className="text-5xl font-semibold font-poppins"
+              >
+                Optimize <br /> Workforce Management
+              </span>
+            </div>
           </div>
+          <video
+            autoPlay
+            loop
+            muted
+            src="vedio2.mp4"
+            type="video/mp4"
+            className=" w-full h-auto max-w-[750px] rounded-tl-[160px] rounded-br-[160px] shadow-lg"
+          ></video>
         </div>
-        <video
-          autoPlay
-          loop
-          muted
-          src="vedio2.mp4"
-          type="video/mp4"
-          className=" w-full h-auto max-w-[750px] rounded-tl-[160px] rounded-br-[160px] shadow-lg"
-        ></video>
-      </div>
       </div>
       {/* rest code */}
       <div className="sm:flex flex-col items-center justify-center lg:hidden bg-[#130c44]">
